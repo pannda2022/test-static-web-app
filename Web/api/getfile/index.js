d4c5442f50335c5dd85e4c3645be979fc8b6bc9e
nta5107179 <39373156+nta5107179@users.noreply.github.com>
2022-10-11 17:49:09 +0900

    Add files via upload

--- Web/api/getfile/index.js
+++ Web/api/getfile/index.js
@@ -1,3 +1,25 @@
+var m_https = require("https");
+    
 module.exports = async function (context, req) {
-    context.res.send(req.query.name);
+    //var url = "http://localhost:60430/" + req.query.name;
+    var url = "https://testblobstorage2022cxw.blob.core.windows.net/testcontainer/" + req.query.name;
+    var type = "";
+    var resData = "";
+    let synchronous_post = function (url, params) {
+        return new Promise(function (resolve, reject) {
+            m_https.get(url, (res)=>{
+                type = res.headers["content-type"]
+                res.setEncoding("binary");
+                res.on("data", (data) => {
+                    resData+=data;
+                }).on("end", () => {
+                    resolve();
+                });
+            });
+        });
+    }
+    await synchronous_post(url);
+
+    context.res.set("Content-Type",type);
+    context.res.send(new Buffer(resData, 'binary'));
 }
