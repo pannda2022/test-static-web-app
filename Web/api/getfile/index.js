519930e340e084767ff991af7f99d4ac1db1044a
nta5107179 <39373156+nta5107179@users.noreply.github.com>
2022-10-11 11:45:26 +0900

    Add files via upload

--- Web/api/getfile/index.js
+++ Web/api/getfile/index.js
@@ -1,11 +1,3 @@
 module.exports = async function (context, req) {
-    context.res.get({
-        url: `https://testblobstorage2022cxw.blob.core.windows.net/testcontainer/test.json`,
-        json: req.body,
-        gzip:true,
-        headers:{
-            'Content-Type': 'application/octet-stream',
-            'usertoken': req.headers.usertoken,
-        },
-    });
+    context.res.download("https://testblobstorage2022cxw.blob.core.windows.net/testcontainer/test.json");
 }
