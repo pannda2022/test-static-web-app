d9d1f4ef0b4179dc1c22694b14310bd0ada2ce73
nta5107179 <39373156+nta5107179@users.noreply.github.com>
2022-10-11 11:49:06 +0900

    Update index.js

--- Web/api/getfile/index.js
+++ Web/api/getfile/index.js
@@ -1,3 +1,5 @@
 module.exports = async function (context, req) {
-    context.res.download("https://testblobstorage2022cxw.blob.core.windows.net/testcontainer/test.json");
+    context.res.json({
+        text: "asdasd"
+    });
 }
